409c6ea50e3edc342443ef9df7f82ee855a2a58c
franantunezt <68174973+franantunezt@users.noreply.github.com>
2020-10-11 19:39:52 -0300

    Quinta entrega

--- js/cart.js
+++ js/cart.js
@@ -3,60 +3,81 @@
 //elementos HTML presentes.
 
 const CART_PRODUCTS = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
-let subtotal = 0;
+let subtotal;
 
 // MUESTRA LOS PRODUCTOS
 function showArticle(array){
-
     let htmlContentToAppend = "";
-
     for(let i = 0; i < array.length; i++){
         article = array[i];
-
         htmlContentToAppend += `
         <tr>
             <td><img src="`+ article.src +`" width="100px"></img> </td>
             <td>`+ article.name +` </td>
             <td><input type="number" min="1" max="100" value="`+ article.count +`" class="quantity" id="quantity"> </td>
             <td>`+article.currency + ` `+ article.unitCost +` </td>
-            
         </tr>
-
-        
         `
     }
     document.getElementById('items').innerHTML = htmlContentToAppend;
     
     // CANTIDAD DE PRODUCTO SELECCIONADO
     let cantidadProducto = document.getElementsByClassName("quantity");
+    let subtotalToAppend = "";
 
     for(i=0; i < cantidadProducto.length; i++){
-        let subtotalToAppend = "";
+        
         console.log(cantidadProducto[i].value);
-        let element = cantidadProducto[i]
+
+        const element = cantidadProducto[i]
         element.onchange = function(e){
             let cantidadtotalproducto = e.target.value
+            subtotal = article.unitCost*cantidadtotalproducto
             console.log(cantidadtotalproducto)
             
-            subtotalToAppend +=`
-            <td id="subtotal">Subtotal: `+ article.unitCost * cantidadtotalproducto +`</td>
+            subtotalToAppend =`
+            <td id="subtotal"><b>`+ subtotal +`<b></td>
 
             `        
-            document.getElementById("subtotal").innerHTML = subtotalToAppend;
+            document.getElementById("subtotal").innerHTML= subtotalToAppend;
         }
     }
+    
+    
 };
 
-function shipping(){
-    document.getElementById
+// Para indicar el shipping
+function check(){
+    let shippingStandard = document.getElementById("standard").checked;
+    let shippingPremium = document.getElementById("premium").checked;
+    let shippingExpress = document.getElementById("express").checked;
+
+    let totalToAppend = "";
+    let total = 0
+
+    if(shippingStandard){
+        total = subtotal*1.05
+        console.log("standard")
+    }else if(shippingPremium){
+        total = subtotal*1.07
+        console.log("premium")
+    }else if(shippingExpress){
+        total = subtotal*1.15
+        console.log("express")
+    }
+    totalToAppend =`
+        <div>`+ Math.round(total) +` </div>`
+
+    document.getElementById("total").innerHTML = totalToAppend;
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(CART_INFO_URL).then(function(resultObj){
         cartInfo = resultObj.data
         let article = cartInfo.articles
-
         showArticle(article);
-
     })
 });
+
+
+console.log(subtotal)
